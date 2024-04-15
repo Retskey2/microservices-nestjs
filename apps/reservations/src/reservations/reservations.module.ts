33a5061ebcf721db8ed8d5d008b9fa72daa16394
RETSKEY <bashlovka96@mail.ru>
2024-04-15 15:14:37 +0300

    Added Validation and System Logging

--- apps/reservations/src/reservations/reservations.module.ts
+++ apps/reservations/src/reservations/reservations.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule, LoggerModule } from '@app/common';
 import { Reservation } from './entities/reservation.entity';
 import { ReservationsRepository } from './reservations.repository';
 
 @Module({
-  imports: [DatabaseModule, DatabaseModule.forFeature([Reservation])],
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([Reservation]),
+    LoggerModule,
+  ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
 })
